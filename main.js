09c43e23f4b7e2983e3505b448877c5cee8f2b35
Dmitry <lereym.99@gmail.com>
2018-09-16 13:26:51 +0500

    add download button

--- main.js
+++ main.js
@@ -48,3 +48,31 @@ function delAll(index) {
     render();
     localStorage.setItem('array', JSON.stringify(array));
 }
+
+function downloadAll() {
+    var content = 'price\n';
+
+    for(var i=0; i < array.length; i++){
+        content += array[i].price + '\n';
+    }
+
+    download(content, 'arraydata.csv', 'text/csv')
+}
+
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
